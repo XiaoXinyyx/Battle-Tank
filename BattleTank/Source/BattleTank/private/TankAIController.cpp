ef304968815d1e1962a55309bca84326dce7eaf5
Yixin_Yang <hyxyyx1996@126.com>
2021-03-19 19:50:44 +0800

    BT 03 创建 TankPlayerController 和 TankAIContoller

---
+++ BattleTank/Source/BattleTank/private/TankAIController.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+
+	// Get AI controlled tank and log out
+	ATank* ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing %s"), *ControlledTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController missing contolled tank"));
+	}
+
+	// Get player contolled tank and log out
+	ATank* PlayerTank = GetPlayerTank();
+	if (PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController find player tank %s"), *PlayerTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIContoller missing player tank"));
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const{
+	return Cast<ATank, APawn>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
